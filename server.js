a11e8913842d9665fafe893fad16813256cfed6b
Dragan Opric <opric.dragan@gmail.com>
2021-02-06 10:37:10 +0100

    solved passing params to redirect with session

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const {check, validationResult}= require('express-validator')
+var session = require('express-session')
+app.use(session({secret: 'mySecret', resave: false, saveUninitialized: false}));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
 app.use(express.static(__dirname + "/public"))
 app.use(express.static(__dirname + "/node_modules/bootstrap/dist"))
 app.set('view engine', 'ejs')
@@ -12,7 +15,10 @@ app.set("views", path.join(__dirname, "views"))
 
 
 app.get('/', (req, res)=>{
-    res.render("index", {message: undefined})
+    console.log("try get index")
+    const msg = req.session.contactmsg;
+    req.session.destroy()
+    res.render("index", {message: msg})
 })
 
 
@@ -28,7 +34,8 @@ app.post("/contact", [
     if (!errors.isEmpty()){
         res.render("contact", {errors: errors.array()})
     }else{
-    res.render("index", {message: `Hi ${req.body.username}, your message has been sent`})
+     req.session.contactmsg = `Hi ${req.body.username}, your message has been sent`;
+    res.redirect('/')
     }
 })
 
